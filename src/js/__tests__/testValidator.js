238132a63a2bbba627ec4f2db216f913ff90e171
Soboleva Natalia <nsoboleva1979@rambler.ru>
2022-04-06 10:53:55 +0300

    corrections have been made

--- src/js/__tests__/testValidator.js
+++ src/js/__tests__/testValidator.js
@@ -6,8 +6,26 @@ test('Тестирование корректного никнейма', () => {
   expect(resolved.validateUsername('zagadka-123a')).toBe(exp);
 });
 
+test('Тестирование корректного никнейма', () => {
+  const exp = 'Никнейм корректен';
+  const resolved = new Validator();
+  expect(resolved.validateUsername('zag13aa_aa')).toBe(exp);
+});
+
+test('Тестирование корректного никнейма', () => {
+  const exp = 'Никнейм корректен';
+  const resolved = new Validator();
+  expect(resolved.validateUsername('zag13aadka-13a')).toBe(exp);
+});
+
 test('Тестирование некорректного никнейма', () => {
   const exp = 'Имя не должно содержать подряд более трёх цифр, а также начинаться и заканчиваться цифрами, символами подчёркивания или тире.';
   const resolved = new Validator();
   expect(resolved.validateUsername('_zagadka-123')).toBe(exp);
+});
+
+test('Тестирование некорректного никнейма', () => {
+  const exp = 'Имя не должно содержать подряд более трёх цифр, а также начинаться и заканчиваться цифрами, символами подчёркивания или тире.';
+  const resolved = new Validator();
+  expect(resolved.validateUsername('z123a1233gadka')).toBe(exp);
 });
--- src/js/validator.js
+++ src/js/validator.js
@@ -1,7 +1,7 @@
 export default class Validator {
   validateUsername(name) {
     this.name = name;
-    if (/^[a-zA-Z]+\w[^.]\d{0,3}[a-zA-Z]$/.test(name)) {
+    if (/^[a-zA-Z]\w[^\d]\d{0,3}[^\d]+\w+[a-zA-Z]$/.test(name)) {
       return 'Никнейм корректен';
     }
     return ('Имя не должно содержать подряд более трёх цифр, а также начинаться и заканчиваться цифрами, символами подчёркивания или тире.');
